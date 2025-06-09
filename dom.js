de6a880d0b7130cf644d476e3f0608730a1d825c
githubscientist <sathish.datascientist@gmail.com>
2025-06-09 21:20:52 +0530

    keyboard event

--- dom.js
+++ dom.js
@@ -1,6 +1,12 @@
-function characterCount(event) {
-    console.log(event.target.value.length);
-}
-
 let nameInput = document.getElementById('registerID');
-nameInput.addEventListener('input', characterCount);
+nameInput.addEventListener('keydown', function (event) {
+    // switch (event.key) {
+    //     case 'c':
+    //         console.log('you pressed ctrl + c');
+    //         break;
+    // }
+
+    if (event.ctrlKey && event.key === 'c') {
+        console.log('you pressed c');
+    }
+});
