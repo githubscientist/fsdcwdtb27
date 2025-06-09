9bbd38d754e9957ddfd37d0552166f17dabafabe
githubscientist <sathish.datascientist@gmail.com>
2025-06-09 20:46:28 +0530

    button click event

--- dom.js
+++ dom.js
@@ -1,5 +1,9 @@
-function handleSubmit() {
-    console.log('button clicked');
+let x = 0;
+function handleSubmit(event) {
+    let colors = ['green', 'blue', 'yellow', 'pink', 'black', 'brown', 'red'];
+    let rIdx = Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor = colors[x++ % 7];
 }
 
 // get the reference of the button with ID btnSubmit
