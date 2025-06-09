8b45236185fe7e26db8491b20f7f1dd14f3c46ed
githubscientist <sathish.datascientist@gmail.com>
2025-06-09 20:19:04 +0530

    addEventListener method

--- dom.js
+++ dom.js
@@ -1,3 +1,9 @@
 function handleSubmit() {
     console.log('button clicked');
 }
+
+// get the reference of the button with ID btnSubmit
+let btnSubmit = document.getElementById('btnSubmit');
+
+// add an event listener to the button
+btnSubmit.addEventListener('click', handleSubmit);
