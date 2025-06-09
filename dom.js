1b2d92827258e8e20365994b2cbaecc585ff2585
githubscientist <sathish.datascientist@gmail.com>
2025-06-09 20:59:35 +0530

    input event

--- dom.js
+++ dom.js
@@ -1,13 +1,6 @@
-let x = 0;
-function handleSubmit(event) {
-    let colors = ['green', 'blue', 'yellow', 'pink', 'black', 'brown', 'red'];
-    let rIdx = Math.floor(Math.random() * colors.length);
-
-    document.body.style.backgroundColor = colors[x++ % 7];
+function characterCount(event) {
+    console.log(event.target.value.length);
 }
 
-// get the reference of the button with ID btnSubmit
-let btnSubmit = document.getElementById('btnSubmit');
-
-// add an event listener to the button
-btnSubmit.addEventListener('click', handleSubmit);
+let nameInput = document.getElementById('registerID');
+nameInput.addEventListener('input', characterCount);
