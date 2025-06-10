7d77a12540f4b1ddb911d40e8bf63667cbdfae93
githubscientist <sathish.datascientist@gmail.com>
2025-06-10 19:37:53 +0530

    navbar --dom api revisited

--- dom.js
+++ dom.js
@@ -1,39 +1,21 @@
-// get the reference of the register form
-const registerForm = document.getElementById('registerForm');
+let menuItems = [
+    { name: 'Home', path: '/home' },
+    { name: 'About', path: '/about' },
+    { name: 'Contact', path: '/contact' },
+    { name: 'Services', path: '/services' },
+    { name: 'Blog', path: '/blog' },
+]
+const navBlock = document.querySelector('.navBlock');
+const navList = document.createElement('ul');
 
-// attach an event listener to the form for submit event
-registerForm.addEventListener('submit', (event) => {
-    // to prevent the default form submission behavior, which would reload the page
-    event.preventDefault();
+menuItems
+    .forEach(menuItem => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = menuItem.name;
+        a.href = menuItem.path;
+        li.appendChild(a);
+        navList.appendChild(li);
+    })
 
-    // get the values of the form elements
-    let name = event.target.name.value;
-    let email = event.target.email.value;
-    let password = event.target.password.value;
-
-    console.log(name, email, password);
-});
-
-function validatePassword(event) {
-    let password = event.target.value;
-
-    let uppercases = 0;
-    for (let i = 0; i < password.length; i++) {
-        if (password[i] >= 'A' && password[i] <= 'Z') {
-            uppercases++;
-        }
-    }
-
-    console.log('password: ', password);
-    console.log('uppercases: ', uppercases);
-
-    if (password.length < 8) {
-        document.getElementById('error').textContent = 'X Password should be atleast 8 characters';
-        document.getElementById('error').style.color = 'red';
-        document.getElementById('registerButton').disabled = true;
-        document.getElementById('error').style.display = "block";
-    } else {
-        document.getElementById('error').style.display = "none";
-        document.getElementById('registerButton').disabled = false;
-    }
-}
+navBlock.append(navList);
