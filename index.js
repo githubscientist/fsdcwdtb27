516e0a225d1d63c913f1b4f54aae360fe3378d05
githubscientist <sathish.datascientist@gmail.com>
2025-06-16 18:57:31 +0530

    GET /all --done

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-/*
-    Fetch
-        - It is a built-in JavaScript function that allows you to make http requests to servers.
-        - It is used to make API calls to retrieve or send data.
-        - Fetch internally uses Promises.
-        - Fetch is an asynchronous function, meaning it does not block the execution of code while waiting for a response.
-        - Fetch is more modern and flexible compared to the older XMLHttpRequest (XHR) method.
+// Global variables or constants
+let todos = [];
+let API_URL = `https://684c3f27ed2578be881e37e6.mockapi.io/todos`;
 
-    XHR
-        - It is an older way to make HTTP requests in JavaScript.
-
-*/
-
-async function fetchData() {
+async function fetchTodos() {
     try {
-        // this is a try block to write code that might throw an error.
-        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/read`);
-        const data = await response.json();
-        console.log(data);
+        // make an api call to the url: https://684c3f27ed2578be881e37e6.mockapi.io/todos
+        const response = await fetch(API_URL);
+
+        // convert the response object to JS object
+        // store the data to the todos variable
+        return response.json();
     } catch (error) {
-        // this is a catch block to handle errors that occur in the try block.
-        console.log('An error occurred:', error);
+        console.log(error);
     }
-    console.log('This will work');
 }
 
-fetchData();
+function renderTodoList(todos) {
+    // identify the ul list item from the main page
+    const todoList = document.querySelector('.todoList');
+
+    // loop through the data
+    todos.forEach(todo => {
+        // for each todo object, create a list item
+        const todoItem = document.createElement('li');
+
+        // set the todoItem content
+        todoItem.textContent = todo.content;
+
+        // append each of the created list item the unordered list
+        todoList.appendChild(todoItem);
+    })
+}
+
+// Entry point for the index.js
+async function Main() {
+    // function to fetch all the todo list items
+    todos = await fetchTodos();
+
+    // populate the page with the data
+    renderTodoList(todos);
+}
+
+Main();
